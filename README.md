b8010d38c192ae8dc9b62c8eb347d53027b6eb4a
Hariadi Hinta <diperakui@yahoo.com>
2013-04-22 21:51:46 +0800

    Initial Yeoman generator for Assemble.

---
+++ README.md
@@ -0,0 +1,13 @@
+# Generator assemble
+[![Build Status](https://secure.travis-ci.org/hariadi/generator-assemble.png?branch=master)](https://travis-ci.org/hariadi/generator-assemble)
+
+Yeoman generator for Assemble.
+
+## Getting started
+- Make sure you have [yo](https://github.com/yeoman/yo) installed:
+    `npm install -g yo`
+- Install the generator **locally**: `npm install generator-assemble`
+- Run: `yo assemble`
+
+## License
+[MIT License](http://en.wikipedia.org/wiki/MIT_License)
---
+++ app/index.js
@@ -0,0 +1,65 @@
+'use strict';
+var util = require('util');
+var path = require('path');
+var yeoman = require('yeoman-generator');
+
+
+var AssembleGenerator = module.exports = function AssembleGenerator(args, options, config) {
+  yeoman.generators.Base.apply(this, arguments);
+
+  this.on('end', function () {
+    this.installDependencies({ skipInstall: options['skip-install'] });
+  });
+
+  this.pkg = JSON.parse(this.readFileAsString(path.join(__dirname, '../package.json')));
+};
+
+util.inherits(AssembleGenerator, yeoman.generators.NamedBase);
+
+AssembleGenerator.prototype.askFor = function askFor() {
+  var cb = this.async();
+
+  // welcome message
+  var welcome =
+  '\n     _-----_' +
+  '\n    |       |' +
+  '\n    |' + '--(o)--'.red + '|   .--------------------------.' +
+  '\n   `---------´  |    ' + 'Welcome to Yeoman,'.yellow.bold + '    |' +
+  '\n    ' + '( '.yellow + '_' + '´U`'.yellow + '_' + ' )'.yellow + '   |   ' + 'ladies and gentlemen!'.yellow.bold + '  |' +
+  '\n    /___A___\\   \'__________________________\'' +
+  '\n     |  ~  |'.yellow +
+  '\n   __' + '\'.___.\''.yellow + '__' +
+  '\n ´   ' + '`  |'.red + '° ' + '´ Y'.red + ' `\n';
+
+  console.log(welcome);
+
+  var prompts = [{
+    name: 'someOption',
+    message: 'Would you like to enable this option?',
+    default: 'Y/n',
+    warning: 'Yes: Enabling this will be totally awesome!'
+  }];
+
+  this.prompt(prompts, function (err, props) {
+    if (err) {
+      return this.emit('error', err);
+    }
+
+    this.someOption = (/y/i).test(props.someOption);
+
+    cb();
+  }.bind(this));
+};
+
+AssembleGenerator.prototype.app = function app() {
+  this.mkdir('app');
+  this.mkdir('app/templates');
+
+  this.copy('_package.json', 'package.json');
+  this.copy('_component.json', 'component.json');
+};
+
+AssembleGenerator.prototype.projectfiles = function projectfiles() {
+  this.copy('editorconfig', '.editorconfig');
+  this.copy('jshintrc', '.jshintrc');
+};
