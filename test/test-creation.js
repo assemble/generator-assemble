fafb1839d69eefefc840deafa83a8403d1de5815
Hariadi Hinta <diperakui@yahoo.com>
2013-04-23 14:35:36 +0800

    Truncated history

---
+++ test/test-creation.js
@@ -0,0 +1,41 @@
+/*global describe, beforeEach, it*/
+'use strict';
+
+var path    = require('path');
+var helpers = require('yeoman-generator').test;
+
+
+describe('assemble generator', function () {
+  beforeEach(function (done) {
+    helpers.testDirectory(path.join(__dirname, 'temp'), function (err) {
+      if (err) {
+        return done(err);
+      }
+
+      this.app = helpers.createGenerator('assemble:app', [
+        '../../app'
+      ]);
+      done();
+    }.bind(this));
+  });
+
+  it('creates expected files', function (done) {
+    var expected = [
+      // add files you expect to exist here.
+      '.jshintrc',
+      '_config.yml',
+      'Gruntfile.js',
+      'dist',
+      'src'
+    ];
+
+    helpers.mockPrompt(this.app, {
+      'someOption': 'Y'
+    });
+
+    this.app.run({}, function () {
+      helpers.assertFiles(expected);
+      done();
+    });
+  });
+});
