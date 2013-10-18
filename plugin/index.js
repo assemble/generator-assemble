2df8ae424368dd1c5890fe793119140c05ee31ec
Hariadi Hinta <diperakui@yahoo.com>
2013-10-18 12:29:59 +0800

    Assemble Plugin Generator
    yo assemble:plugin -h or See
    [Usage](https://github.com/assemble/generator-assemble/tree/master/plugin/USAGE)
    
    Fixed #17 and closed assemble/grunt-init-assemble-plugin#1

---
+++ plugin/index.js
@@ -0,0 +1,30 @@
+'use strict';
+var util = require('util');
+var yeoman = require('yeoman-generator');
+
+var PluginGenerator = module.exports = function PluginGenerator(args, options, config) {
+
+  //args.push('assemble:plugin');
+
+  if(Object.keys(args).length === 0){
+    args.push('my_plugin');
+    args.push('./plugins');
+  };
+
+  yeoman.generators.NamedBase.apply(this, arguments);
+  this.name = this.name || args[0];
+  this.path = this.path || args[1];
+
+};
+
+util.inherits(PluginGenerator, yeoman.generators.NamedBase);
+
+PluginGenerator.prototype.create = function create() {
+
+  this.pluginName = this.name;
+  this.authorLogin = this.config.get("githubUser");
+  this.authorName = this.config.get("author").name;
+  this.authorEmail = this.config.get("author").email;
+  this.pluginPath = this.path;
+  this.template('plugin.js', this.pluginPath + '/assemble-plugin-'+ this._.dasherize(this.pluginName) +'.js');
+};
