7a140ff8b7c2fa1d80a8ca26e78f6c01c000909e
Steven 'Diagram' Sinatra <ohai@diagramatics.me>
2014-06-21 05:11:08 +1000

    Revert "Use load-grunt-tasks"
    This reverts commit 0b91f9021b3741225a27608771ffc8bb29cd525d.

--- app/templates/Gruntfile.js
+++ app/templates/Gruntfile.js
@@ -19,8 +19,6 @@ module.exports = function(grunt) {
 
   require('time-grunt')(grunt);
 
-  require('load-grunt-tasks')(grunt);
-
   // Project configuration.
   grunt.initConfig({
 
@@ -94,6 +92,11 @@ module.exports = function(grunt) {
     clean: ['<%%= config.dist %>/**/*.{html,xml}']
 
   });
+
+  grunt.loadNpmTasks('assemble');
+  grunt.loadNpmTasks('grunt-contrib-clean');
+  grunt.loadNpmTasks('grunt-contrib-connect');
+  grunt.loadNpmTasks('grunt-contrib-watch');
 
   grunt.registerTask('server', [
     'clean',
